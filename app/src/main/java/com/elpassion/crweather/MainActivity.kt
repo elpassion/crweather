034394aff59cd5a81c8a133ed101372ea354753f
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-06-07 18:11:17 +0200

    Clean up default app template a bit

--- app/src/main/java/com/elpassion/crweather/MainActivity.kt
+++ app/src/main/java/com/elpassion/crweather/MainActivity.kt
@@ -2,11 +2,10 @@ package com.elpassion.crweather
 
 import android.os.Bundle
 import android.support.design.widget.NavigationView
-import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
+import android.util.Log
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
@@ -17,12 +16,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-        val toggle = ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer.addDrawerListener(toggle)
         toggle.syncState()
         navigation.setNavigationItemSelectedListener(this)
@@ -34,25 +28,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onBackPressed()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.action_settings -> true
-        else -> super.onOptionsItemSelected(item)
-    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            R.id.nav_camera -> Unit
-            R.id.nav_gallery -> Unit
-            R.id.nav_slideshow -> Unit
-            R.id.nav_manage -> Unit
-            R.id.nav_share -> Unit
-            R.id.nav_send -> Unit
-        }
+        Log.w("CRW", "TODO: convert \"$item\" to user action and send to view model")
         drawer.closeDrawer(GravityCompat.START)
         return true
     }
