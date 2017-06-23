b94632cc7207e3052d4249a9ae07f5d398696d92
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-06-23 10:55:30 +0200

    Move static chart utils out of ChartView class

--- app/src/main/java/com/elpassion/crweather/ChartView.kt
+++ app/src/main/java/com/elpassion/crweather/ChartView.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.channels.Channel
 import kotlinx.coroutines.experimental.channels.actor
 import kotlinx.coroutines.experimental.delay
-import java.lang.Math.signum
 
 
 class ChartView @JvmOverloads constructor(
@@ -23,56 +22,22 @@ class ChartView @JvmOverloads constructor(
         }
 
     val actor = actor<Chart>(UI, Channel.CONFLATED) {
+
         var currentChart = chart.deepCopy()
-        var currentVelocities = chart.deepCopy().stopVelocities()
+        var currentVelocities = chart.deepCopy().resetPoints()
+
         doOnDraw { drawChart(currentChart) }
-        for (newChart in this) {
-            currentChart = currentChart.copyAndReformat(newChart)
-            currentVelocities = currentVelocities.copyAndReformat(newChart)
+
+        for (destinationChart in this) {
+
+            currentChart = currentChart.copyAndReformat(destinationChart)
+            currentVelocities = currentVelocities.copyAndReformat(destinationChart)
+
             while (isActive && isEmpty) {
                 redraw()
                 delay(10)
-                currentChart.mutateOneStepTowards(destination = newChart, velocities = currentVelocities)
+                currentChart.moveABitTo(destinationChart, currentVelocities)
             }
         }
-    }
-
-    companion object {
-
-        private fun Chart.stopVelocities() = apply { lines.forEach { it.points.forEach { it.x = 0f; it.y = 0f } } }
-
-        private fun Chart.mutateOneStepTowards(destination: Chart, velocities: Chart) {
-            for ((lineIdx, line) in lines.withIndex())
-                for ((pointIdx, point) in line.points.withIndex()) {
-                    val velocityPoint = velocities.lines[lineIdx].points[pointIdx]
-                    val destinationPoint = destination.lines[lineIdx].points[pointIdx]
-                    point.x += velocityPoint.x
-                    point.y += velocityPoint.y
-                    velocityPoint.x = computeNewVelocity(velocityPoint.x, point.x, destinationPoint.x)
-                    velocityPoint.y = computeNewVelocity(velocityPoint.y, point.y, destinationPoint.y)
-                }
-        }
-
-        private fun computeNewVelocity(velocity: Float, currentPosition: Float, destinationPosition: Float): Float {
-            var newVelocity = velocity + (destinationPosition - currentPosition) / 20f
-            if (signum(newVelocity) != signum(destinationPosition - currentPosition))
-                newVelocity = newVelocity / 1.5f
-            return newVelocity
-        }
-
-        private fun Chart.copyAndReformat(model: Chart) = Chart(model.inputRange, model.outputRange, lines.copyAndReformatLines(model.lines), model.timeMs)
-
-        private fun List<Line>.copyAndReformatLines(model: List<Line>) = MutableList(model.size) { idx ->
-            getOrElse(idx) { model[idx] }.copyAndReformat(model[idx])
-        }
-
-        private fun Line.copyAndReformat(model: Line) = Line(model.name, model.color, points.copyAndReformatPoints(model.points))
-
-        private fun List<Point>.copyAndReformatPoints(model: List<Point>) = MutableList(model.size) { idx ->
-            getOrElse(idx) { model[idx] }.copy()
-        }
-
-        private fun Chart.deepCopy() = copy(lines = lines.map { it.deepCopy() })
-        private fun Line.deepCopy() = copy(points = points.map { it.copy() })
     }
 }
--- app/src/main/java/com/elpassion/crweather/ChartsUtils.kt
+++ app/src/main/java/com/elpassion/crweather/ChartsUtils.kt
@@ -2,6 +2,7 @@ package com.elpassion.crweather
 
 import android.graphics.Color
 import com.elpassion.crweather.OpenWeatherMapApi.DailyForecast
+import java.lang.Math.signum
 
 private val CACHE_TIME = 1000L * 60L * 60L // one hour
 
@@ -73,3 +74,40 @@ private fun List<DailyForecast>.toPoints(toValue: DailyForecast.() -> Float?)
 private fun DailyForecast.toPointOrNull(toValue: DailyForecast.() -> Float?)
         = toValue()?.let { Point(dt.toFloat(), it) }
 
+
+fun Chart.deepCopy() = copy(lines = lines.map { it.deepCopy() })
+
+fun Line.deepCopy() = copy(points = points.map { it.copy() })
+
+fun Chart.resetPoints() = apply { lines.forEach { it.points.forEach { it.x = 0f; it.y = 0f } } }
+
+fun Chart.moveABitTo(destination: Chart, velocities: Chart) {
+    for ((lineIdx, line) in lines.withIndex())
+        for ((pointIdx, point) in line.points.withIndex()) {
+            val velocityPoint = velocities.lines[lineIdx].points[pointIdx]
+            val destinationPoint = destination.lines[lineIdx].points[pointIdx]
+            point.x += velocityPoint.x
+            point.y += velocityPoint.y
+            velocityPoint.x = updateVelocity(velocityPoint.x, point.x, destinationPoint.x)
+            velocityPoint.y = updateVelocity(velocityPoint.y, point.y, destinationPoint.y)
+        }
+}
+
+private fun updateVelocity(velocity: Float, currentPosition: Float, destinationPosition: Float): Float {
+    var newVelocity = velocity + (destinationPosition - currentPosition) / 20f
+    if (signum(newVelocity) != signum(destinationPosition - currentPosition))
+        newVelocity = newVelocity / 1.5f
+    return newVelocity
+}
+
+fun Chart.copyAndReformat(model: Chart) = Chart(model.inputRange, model.outputRange, lines.copyAndReformatLines(model.lines), model.timeMs)
+
+private fun List<Line>.copyAndReformatLines(model: List<Line>) = MutableList(model.size) { idx ->
+    getOrElse(idx) { model[idx] }.copyAndReformat(model[idx])
+}
+
+private fun Line.copyAndReformat(model: Line) = Line(model.name, model.color, points.copyAndReformatPoints(model.points))
+
+private fun List<Point>.copyAndReformatPoints(model: List<Point>) = MutableList(model.size) { idx ->
+    getOrElse(idx) { model[idx] }.copy()
+}
